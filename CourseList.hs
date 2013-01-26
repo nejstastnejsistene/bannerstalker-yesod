7708702b1d98746de65edbe7f5577ef2f33bf251
Peter Johnson <pajohnson@email.wm.edu>
2013-01-25 23:56:52 -0500

    Change TagSoup to a qualified import so I could use the identifier 'sections', move the data declarations to the top, and added a few function level comments.

--- CourseList.hs
+++ CourseList.hs
@@ -1,12 +1,26 @@
 import Data.Char
 import Data.Maybe
-import Text.HTML.TagSoup
 import Network.HTTP
 import Network.URI
+import qualified Text.HTML.TagSoup as TS
+
+data SectionStatus = Open | Closed deriving (Show)
+data Section = Unavailable |
+               SectionData { semester :: String
+                           , crn :: Int
+                           , subject :: String
+                           , courseId :: String
+                           , title :: String
+                           , instructor :: String
+                           , days :: String
+                           , times :: String
+                           , status :: Maybe SectionStatus
+                           } deriving (Show)
 
 uri = fromJust $ parseURI url where
 url = "http://courselist.wm.edu/wmcourseschedule/courseinfo/searchresults"
 
+-- Requests the raw html from the courselist website.
 requestCourseList :: String -> String -> IO (Either String String)
 requestCourseList semester subject = do
     response <- simpleHTTP request
@@ -35,38 +49,26 @@ requestCourseList semester subject = do
             rqHeaders = headers,
             rqBody = postData }
      
+-- Parses the raw html into a table of strings from the td tags.
 parseCourseList :: String -> [[String]]
 parseCourseList html = do
     extractTagText rows
     where
-        table = head $ getTagContents "table" $ parseTags html
+        table = head $ getTagContents "table" $ TS.parseTags html
         rows = map (getTagContents "td") $ getTagContents "tr" table
         -- Extension of sections that trims tags coming after
         -- the first closing tag.
-        getTagContents :: String -> [Tag String] -> [[Tag String]]
+        getTagContents :: String -> [TS.Tag String] -> [[TS.Tag String]]
         getTagContents tagName tags =
-            let sectionList = sections (~== ("<"++tagName++">")) tags
+            let sections = TS.sections (TS.~== ("<"++tagName++">")) tags
                 -- Take only the tags between the start and end tags.
-                trimTags = takeWhile (not . isTagCloseName tagName)
-                in map (trimTags . tail) sectionList 
+                trimTags = takeWhile (not . TS.isTagCloseName tagName)
+                in map (trimTags . tail) sections 
         -- Converts the list of TagTexts to 2d array of strings
-        extractTagText :: [[[Tag String]]] -> [[String]]
+        extractTagText :: [[[TS.Tag String]]] -> [[String]]
         extractTagText rows = 
-            let extract = map $ map $ head . (map fromTagText)
+            let extract = map $ map $ head . (map TS.fromTagText)
             in filter (\x -> x /= []) $ (extract rows)
-
-data SectionStatus = Open | Closed deriving (Show)
-data Section = Unavailable |
-               SectionData { semester :: String
-                           , crn :: Int
-                           , subject :: String
-                           , courseId :: String
-                           , title :: String
-                           , instructor :: String
-                           , days :: String
-                           , times :: String
-                           , status :: Maybe SectionStatus
-                           } deriving (Show)
 
 -- Creates a Section given the semester and a list of arguments.
 makeSection :: String -> [String] -> Either String Section
@@ -91,5 +93,5 @@ main = let semester = "201320" in do
         Left err  -> putStrLn $ "Error:\n" ++ err
         Right html ->
             let rows = parseCourseList html
-                x = map (makeSection semester) rows
-            in putStrLn $ show x
+                sections = map (makeSection semester) rows
+            in putStrLn $ show sections
