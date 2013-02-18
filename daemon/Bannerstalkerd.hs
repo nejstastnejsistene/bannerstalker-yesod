3e1bd218eaa16179e15687c5621c7af09ef25599
Peter Johnson <pajohnson@email.wm.edu>
2013-02-18 00:12:11 -0500

    Prepped to begin working on bannerstalkerd for real now; enough fucking around with monads.

--- daemon/Bannerstalkerd.hs
+++ daemon/Bannerstalkerd.hs
@@ -1,37 +1,23 @@
-module Bannerstalkerd where
-
+module Bannerstalkerd where 
 import Import
 import Prelude
 import Database.Persist
-import Database.Persist.Store
-import Database.Persist.GenericSql
 import Database.Persist.Postgresql
-import System.Posix.Daemonize
-import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (runResourceT)
 
 import CourseList
 import Model
 import Settings
 
-{-daemon :: CreateDaemon ()
-daemon = CreateDaemon {  privilegedAction = return ()
-                      ,  program = const bannerstalkerd
-                      ,  name = Just "bannerstalkerd"
-                      ,  user = Just "bannerstalker"
-                      ,  group = Just "bannerstalker"
-                      ,  syslogOptions = []
-                      ,  pidfileDirectory = Just "/var/run"
-                      }
--}
-
---bannerstalkerd :: (MonadIO m) => [Section] -> SqlPersist m ()
-{-bannerstalkerd sections = do
-    let firstSection = head sections
-    sectionId <- insert firstSection    
-    liftIO $ putStrLn $ show sectionId -}
-
-    -- sections from db
+bannerstalkerd :: PersistConfig -> IO ()
+bannerstalkerd dbConf = do
+    let conn = withPostgresqlConn (pgConnStr dbConf)
+    runResourceT $ conn $ runSqlConn $ do 
+        -- sections from db
+        sections <- selectList [] []
+        liftIO $ putStrLn $ show (sections :: [Entity Section])
+    return ()
+    --sections <- runPool dbConf (do selectList [] []) pool
     -- statuses from db
     -- loop
         -- keys
@@ -45,10 +31,11 @@ daemon = CreateDaemon {  privilegedAction = return ()
             -- if key in statuses and old status != new status
                 -- notify users
 
+{-
 startDaemon :: Settings.PersistConfig -> IO ()
 --startDaemon = serviced daemon
 startDaemon conf = do
-    putStrLn $ show $ pgConnStr conf
+    --putStrLn $ show $ pgConnStr conf
     courseList <- fetchCourseList "201320" "0"
     case courseList of
         Left err -> do
@@ -61,16 +48,4 @@ startDaemon conf = do
                 deleteWhere [SectionSemester ==. "201320"]
                 sectionIds <- mapM insert sections    
                 liftIO $ putStrLn $ show sectionIds 
-    {-pool <- createPoolConfig conf 
-    courseList <- fetchCourseList "201320" "MATH"
-    case courseList of
-        Left err -> putStrLn "error"
-        Right sections -> do
-            let section = head section
-            putStrLn $ show section
-            runPool conf asdf-}
-
-asdf section = do
-    runMigration migrateAll
-    insert section
-    return ()
+-}
--- daemon/Main.hs
+++ daemon/Main.hs
@@ -1,11 +1,27 @@
-import Prelude              (IO)
-import Yesod.Default.Config (fromArgs)
-import Application          (getPersistConfig)
-import Settings             (parseExtra)
-import Bannerstalkerd       (startDaemon)
+import Prelude                  (IO)
+import Yesod.Default.Config     (fromArgs)
+import Application              (getPersistConfig)
+import Settings                 (parseExtra)
+import Bannerstalkerd           (bannerstalkerd)
+import System.Posix.Daemonize   (CreateDaemon, serviced)
 
-main :: IO ()
-main = do
+daemon :: CreateDaemon ()
+daemon = CreateDaemon {  privilegedAction = return ()
+                      ,  program = const startDaemon
+                      ,  name = Just "bannerstalkerd"
+                      ,  user = Just "bannerstalker"
+                      ,  group = Just "bannerstalker"
+                      ,  syslogOptions = []
+                      ,  pidfileDirectory = Just "/var/run"
+                      }
+
+startDaemon :: IO ()
+startDaemon = do
     conf <- fromArgs parseExtra
     dbConf <- getPersistConfig conf
-    startDaemon dbConf
+    bannerstalkerd dbConf
+
+
+main :: IO ()
+--main = serviced daemon
+main = startDaemon
