090f0ad14f011953645a877faffa36c441d09423
Peter Johnson <pajohnson@email.wm.edu>
2013-02-28 22:27:12 -0500

    Fixed up courselist a bit using experienced gained from that nasty memory bug.

--- daemon/CourseList.hs
+++ daemon/CourseList.hs
@@ -1,16 +1,13 @@
 module CourseList (fetchCourseList) where
 
 import Prelude
-import Control.Exception
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as B
 import Data.Conduit
+import Data.Conduit.List (consume)
 import Data.Either
 import Data.Maybe
 import qualified Data.Text as T
-import Data.Text.Encoding (encodeUtf8)
-import Data.Text.Lazy (toStrict)
-import Data.Text.Lazy.Encoding (decodeUtf8)
+import Data.Text.Encoding
 import Text.Regex.PCRE
 import Network.HTTP.Conduit
 import Network.HTTP.Types
@@ -21,7 +18,7 @@ url :: String
 url = "http://courselist.wm.edu/wmcourseschedule/courseinfo/searchresults"
 
 -- Requests the raw html from the courselist website.
-requestCourseList :: Manager -> T.Text -> IO (Either T.Text BL.ByteString)
+requestCourseList :: Manager -> T.Text -> IO (Either T.Text B.ByteString)
 requestCourseList manager semester = do
     runResourceT $ do
         defReq <- parseUrl url
@@ -29,13 +26,14 @@ requestCourseList manager semester = do
                         { checkStatus = \_ _ -> Nothing
                         , responseTimeout = Just 60000000 -- 1 minute
                         }
-        response <- httpLbs request manager
-        let status = responseStatus response
-        case status of
-            Status 200 "OK" -> return $ Right $ responseBody response
-            _ -> return $ Left $ T.pack $ show status
+        response <- http request manager
+        body <- fmap B.concat $ responseBody response $$+- consume
+        case responseStatus response of
+            Status 200 "OK" -> return $ Right body
+            Status code mesg -> return $ Left $ T.pack $
+                "Status: " ++ show code ++ " " ++ show mesg
     where
-        params :: [(BS.ByteString, BS.ByteString)]
+        params :: [(B.ByteString, B.ByteString)]
         params = [("term_code", encodeUtf8 semester)
                  ,("term_subj", "0")
                  ,("attr",      "0")
@@ -46,8 +44,8 @@ requestCourseList manager semester = do
                  ,("order",     "asc")]
      
 -- Creates a Section given the semester and a list of arguments.
-makeSection :: T.Text -> [BL.ByteString] -> Either T.Text Section
-makeSection semester args = case args' of
+makeSection :: T.Text -> [B.ByteString] -> Either T.Text Section
+makeSection semester args = case map (T.strip . decodeUtf8) args of
     [crn, courseId, _, title, instr,  _, days, times, _, _, _, status] ->
         let crn' = read $ T.unpack crn
             subject:courseId':_ = T.words courseId
@@ -59,29 +57,24 @@ makeSection semester args = case args' of
             then Left $ T.concat ["Unkown status: ", status]
             else Right $ Section semester crn' subject courseId'
                             title instr days times $ fromJust status'
-    _ -> Left $ T.concat [ "Wrong number of arguments to makeSection: "
-                         , T.pack $ show (args' :: [T.Text])
-                         ]
-    where args' = map (T.strip . toStrict . decodeUtf8) args
+    _ -> Left "Wrong number of arguments to makeSection"
 
 fetchCourseList :: Manager -> T.Text -> IO (Either T.Text [Section])
 fetchCourseList manager semester = do
     response <- requestCourseList manager semester
-    case response of
-        Left err -> return $ Left err
+    return $ case response of
+        Left err -> Left err
         Right html ->
             let rows = map tail $ html =~ pattern
                 eitherSections = map (makeSection semester) rows
                 (errors, sections) = partitionEithers eitherSections
             in case errors of
-                [] -> return $ Right sections
-                _  -> do
-                    putStrLn $ show $ head errors
-                    return $ Left $ T.concat
-                        [ T.pack $ show $ length errors
-                        , " errors: e.g. "
-                        , T.pack $ show $ head errors
-                        ]
+                [] -> Right sections
+                _  -> Left $ T.concat
+                    [ T.pack $ show $ length errors
+                    , " errors: e.g. "
+                    , T.pack $ show $ head errors
+                    ]
     where
-        pattern :: BL.ByteString
-        pattern = BL.concat $ replicate 12 "<td[^>]*>([^<]+)</td>\\s*"
+        pattern :: B.ByteString
+        pattern = B.concat $ replicate 12 "<td[^>]*>([^<]+)</td>\\s*"
