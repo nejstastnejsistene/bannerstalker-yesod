a695c981d98c22686c12c4a177c49f77125ae5ea
Peter Johnson <pajohnson@email.wm.edu>
2013-03-03 21:54:13 -0500

    Added back links to the homepage.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -12,10 +12,13 @@ getHomeR = do
     case mUser of
         Just user -> defaultLayout [whamlet|
 <h1>Welcome, #{userEmail user}
+<a href=@{QuickstartR}>Quickstart
+<a href=@{LogoutR}>Logout
 |]
         Nothing -> defaultLayout [whamlet|
 <h1>not logged in
 <a href=@{RegisterR}>Register
+<a href=@{LoginR}>Login
 |]
 
 getQuickstartR :: Handler RepHtml
