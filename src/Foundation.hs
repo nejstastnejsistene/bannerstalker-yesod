1b98e127a67eaa8d350b38f528d2eb2201cd5eb5
Peter Johnson <pajohnson@email.wm.edu>
2013-03-13 13:13:08 -0400

    Added Font Awesome.

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -122,6 +122,7 @@ instance Yesod App where
 
         pc <- widgetToPageContent $ do
             addStylesheet $ StaticR css_bootstrap_css
+            addStylesheet $ StaticR css_font_awesome_min_css
             $(widgetFile "default-layout")
         hamletToRepHtml $(hamletFile "templates/default-layout-wrapper.hamlet")
 
