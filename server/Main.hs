f97c9bc1e203c4bee6cd3eea1e8ad904df066638
Peter Johnson <pajohnson@email.wm.edu>
2013-03-22 21:30:19 -0400

    Made it parse the argument env.

--- server/Main.hs
+++ server/Main.hs
@@ -2,6 +2,7 @@ import Prelude              (IO)
 import Yesod.Default.Config
 import Yesod.Default.Main   (defaultMain)
 import Application          (getAppConfig, makeApplication)
+import Settings             (parseExtra)
 
 main :: IO ()
-main = defaultMain (getAppConfig Development) makeApplication
+main = defaultMain (fromArgs parseExtra) makeApplication
