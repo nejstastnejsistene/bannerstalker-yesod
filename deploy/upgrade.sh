5ffade0606eda0f150107b9404c20291f99f89b1
Peter Johnson <pajohnson@email.wm.edu>
2013-03-25 12:32:03 -0400

    Delete the dist and static/tmp directories on upgrade to ensure css is updated.

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -4,7 +4,7 @@ LOGIN_NAME=ubuntu
 for server in `deploy/list-instances.py`; do
     echo deploying to $server
     ssh $SSH_OPTS $LOGIN_NAME@$server sudo service bannerstalker stop
-    ssh $SSH_OPTS $LOGIN_NAME@$server "cd bannerstalker && git fetch origin && git reset --hard origin/master"
+    ssh $SSH_OPTS $LOGIN_NAME@$server "cd bannerstalker && sudo rm -rf dist/ static/tmp/ && git fetch origin && git reset --hard origin/master"
     scp $SSH_OPTS dist/build/bannerstalker/bannerstalker $LOGIN_NAME@$server:bannerstalker
     ssh $SSH_OPTS $LOGIN_NAME@$server sudo service bannerstalker start
 done
